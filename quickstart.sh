d1f81a00ab5bc01ca49e771a44e90079d34dfae7
Thomas Wolgast Rørbech <thwr@itu.dk>
2024-03-20 20:01:55 +0100

    add quick start script

---
+++ quickstart.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+SERVERS=${1:-4}
+
+# Build the Docker image
+docker build -t my-haproxy .
+
+# Run the HAProxy container
+docker run -d -p 8080:80 --name my-running-haproxy --sysctl net.ipv4.ip_unprivileged_port_start=0 my-haproxy
+
+# Execute a command in a running container
+docker exec -it my-running-haproxy sh -c "python /usr/local/src/main.py -s $SERVERS"
